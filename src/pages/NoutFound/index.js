79bbde5e6a9ae7cbbda433f3128e2acacd086359
daniel-dantas <daniel.dantas.developer@gmail.com>
2020-09-09 00:29:05 -0300

    env: Organizando diretório do projeto, e configurando rotas

---
+++ src/pages/NoutFound/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function NoutFound() {
+  return (
+    <div id="notfound">
+      <h1>Page Not Found</h1>
+    </div>
+  );
+}
+
+export default NoutFound;
